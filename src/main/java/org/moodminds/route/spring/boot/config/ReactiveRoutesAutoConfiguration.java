c8f43bc2d3cad43b5b20b0771fe3edd92e99ec51
Ivan Tishchenko <moodminds@hotmail.com>
2025-06-26 10:57:22 +0300

    Adjust TraverseRoutesAutoConfiguration.java, ReactiveRoutesAutoConfiguration.java - cosmetic.

--- src/main/java/org/moodminds/route/spring/boot/config/ReactiveRoutesAutoConfiguration.java
+++ src/main/java/org/moodminds/route/spring/boot/config/ReactiveRoutesAutoConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.REACTIVE;
@@ -15,7 +14,6 @@ import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebA
 /**
  * The {@link Routes} autoconfiguration bean.
  */
-@Configuration
 @ConditionalOnWebApplication(type = REACTIVE)
 @ConditionalOnClass(Routes.class)
 @Import(ReactiveAdapterRegistration.class)
